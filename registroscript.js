0db5aa4b250c700140873128240ef130878a8508
Enginyers <86854121+Enginyers@users.noreply.github.com>
2022-02-17 15:47:40 -0500

    validacion formulario

--- registroscript.js
+++ registroscript.js
@@ -1,25 +1,40 @@
+
 function registrar(){
- 
-   var form = document.getElementById("formulario").elements;
- 
+
+    
+    var nombre = document.getElementById('txtNombre').value;
+    var apellido = document.getElementById('txtApellido').value;
+    var telefono = document.getElementById('txtTelefono').value;
+    var direccion = document.getElementById('txtDireccion').value;
+
+
+if(nombre.length > 0 && apellido.length > 0 && telefono.length > 0 && direccion.length > 0 )
+   {
+    
+    
+    var form = document.getElementById("formulario").elements;
+
 for(var i = 0; i<= form.length - 1; i++)
 {
 
-    if(form[i].type != 'button' )   
+    if(form[i].type != 'submit' )   
     {
         //imprimir en consola el valor
         console.log(form[i].value);
         localStorage.setItem(form[i].id, JSON.stringify(form[i].value));
     }
   
-} 
+}
 
- if (localStorage.length > 0) 
-    {
-        window.alert("Registro exitoso");
-       
-    } 
+    window.alert("Registro exitoso");
+  }
+
+
 
 }
 
-console.log(localStorage);
+console.log(localStorage.getItem('txtNombre')); 
+console.log(localStorage.getItem('txtApellido')); 
+console.log(localStorage.getItem('txtTelefono'));
+console.log(localStorage.getItem('txtDireccion'));
+
